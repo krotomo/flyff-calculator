24a5f217e30ce9c0436566e5f2aaed248d9bc05d
Ken Otomo <ken.otomo@stonybrook.edu>
2024-03-04 23:29:40 -0500

    started shadcn form component usage and zod

--- src/components/pet-input.tsx
+++ src/components/pet-input.tsx
@@ -1,4 +1,6 @@
 import { useForm, useFieldArray, Controller } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { NumericFormat } from 'react-number-format';
 import { PatternFormat } from 'react-number-format';
 import { Input } from "@/components/ui/input"
@@ -10,21 +12,29 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 
-
-type FormValues = {
-  petType: string,
-  levels: string,
-  statGoal: string,
-  sacPrice: {
-    tier: string,
-    price: string,
-  }[],
-  candyPrice: {
-    tier: string,
-    price: string,
-  }[],
-}
+const formSchema = z.object({
+  petType: z.string(),
+  levels: z.string(),
+  statGoal: z.string(),
+  sacPrice: z.array(z.object({
+    tier: z.string(),
+    price: z.string(),
+  })),
+  candyPrice: z.array(z.object({
+    tier: z.string(),
+    price: z.string(),
+  }))
+})
 
 const statRange: { [key: string]: { min: number, max: number } } = {
   "unicorn": { min: 78, max: 7182 },
@@ -47,28 +57,6 @@ function PetInput({ setPetState }: {
     candyPrices: {[key: string]: number}
   ) => void;
 }) {
-  const defaultValues : FormValues = {
-    petType: "",
-    levels: "1",
-    statGoal: "",
-    sacPrice: [
-      { tier: "e", price: "6000000" },
-      { tier: "d", price: "20000000" },
-      { tier: "c", price: "57500000" },
-      { tier: "b", price: "120000000" },
-      { tier: "a", price: "220000000" },
-      { tier: "s", price: "545000000" },
-    ],
-    candyPrice: [
-      { tier: "f", price: "200000" },
-      { tier: "e", price: "700000" },
-      { tier: "d", price: "1500000" },
-      { tier: "c", price: "2500000" },
-      { tier: "b", price: "4000000" },
-      { tier: "a", price: "6500000" },
-    ]
-  }
-
   const statNameByPetType: { [key: string]: string } = {
     "unicorn": "HP",
     "dragon": "Attack",
@@ -81,21 +69,42 @@ function PetInput({ setPetState }: {
     "fox": "INT",
   }
 
-  const { control, register, watch, handleSubmit } = useForm({
-    defaultValues: defaultValues
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      petType: "",
+      levels: "",
+      statGoal: "",
+      sacPrice: [
+        { tier: "e", price: "6000000" },
+        { tier: "d", price: "20000000" },
+        { tier: "c", price: "57500000" },
+        { tier: "b", price: "120000000" },
+        { tier: "a", price: "220000000" },
+        { tier: "s", price: "545000000" },
+      ],
+      candyPrice: [
+        { tier: "f", price: "200000" },
+        { tier: "e", price: "700000" },
+        { tier: "d", price: "1500000" },
+        { tier: "c", price: "2500000" },
+        { tier: "b", price: "4000000" },
+        { tier: "a", price: "6500000" },
+      ]
+    }
   })
   const { fields: sacPriceFields } = useFieldArray({
-    control: control,
+    control: form.control,
     name: "sacPrice",
   })
   const { fields: candyPriceFields } = useFieldArray({
-    control: control,
+    control: form.control,
     name: "candyPrice",
   })
 
-  const petType = watch("petType")
+  const petType = form.watch("petType")
 
-  function onSubmit(formData : FormValues) {
+  function onSubmit(formData: z.infer<typeof formSchema>) {
     const levels = []
     for (const level of formData.levels) {
       if (!isNaN(parseInt(level))) {
@@ -122,47 +131,50 @@ function PetInput({ setPetState }: {
 
   return(
     <div>
-      <form onSubmit={handleSubmit((formData) => onSubmit(formData))}>
-        <h2>Pet Info</h2>
-        <div>
-          <Label>Pet Type</Label>
-          <Controller
-            control={control}
-            name="petType"
-            render={
-              ({ field: { onChange, value } }) => (
-                <Select
-                  onValueChange={onChange}
-                  value={value}
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select Pet"/>
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="unicorn">Unicorn</SelectItem>
-                    <SelectItem value="dragon">Dragon</SelectItem>
-                    <SelectItem value="griffin">Griffin</SelectItem>
-                    <SelectItem value="angel">Angel</SelectItem>
-                    <SelectItem value="crab">Crab</SelectItem>
-                    <SelectItem value="tiger">Tiger</SelectItem>
-                    <SelectItem value="lion">Lion</SelectItem>
-                    <SelectItem value="rabbit">Rabbit</SelectItem>
-                    <SelectItem value="fox">Fox</SelectItem>
-                  </SelectContent>
-                </Select>     
-              )
-            }
-          />
-        </div>
-        <div>
-          <Controller
-            control={control}
-            name="levels"
-            render={
-              ({ field: { onChange, value } }) => (
-                <div>
-                  <Label>Levels</Label>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <h2>Pet Info</h2>
+          <div>
+            <FormField
+              control={form.control}
+              name="petType"
+              render={
+                ({ field }) => (  
+                  <FormItem>
+                    <FormLabel>Pet Type</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Pet Type"/>
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="unicorn">Unicorn</SelectItem>
+                        <SelectItem value="dragon">Dragon</SelectItem>
+                        <SelectItem value="griffin">Griffin</SelectItem>
+                        <SelectItem value="angel">Angel</SelectItem>
+                        <SelectItem value="crab">Crab</SelectItem>
+                        <SelectItem value="tiger">Tiger</SelectItem>
+                        <SelectItem value="lion">Lion</SelectItem>
+                        <SelectItem value="rabbit">Rabbit</SelectItem>
+                        <SelectItem value="fox">Fox</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>      
+                )
+              }
+            />
+          </div>
+          <div>
+            <Label htmlFor="levels">Levels</Label>
+            <Controller
+              control={form.control}
+              name="levels"
+              render={
+                ({ field: { onChange, value } }) => (            
                   <PatternFormat 
+                    id="levels"
                     onChange={onChange}
                     value={value}
                     type="text"
@@ -170,94 +182,103 @@ function PetInput({ setPetState }: {
                     format="#/#/#/#/#/#/#"
                     customInput={Input}
                   />
-                </div>
-              )
-            }
-          />
-        </div>
-        <div>
-          <Label>Stat Goal</Label>
-          <Input
-            {...register(
-              "statGoal", 
-              {
-                required: true,
-                min: 0,
+                )
               }
-            )}
-            type="text"
-            inputMode="numeric"
-          />
-        </div>
-        <h2>Prices</h2>
-        <h3>Sac Pets</h3>
-        <div>
-          {
-            sacPriceFields.map((field, index: number) => {
-              return (              
-                <div key={field.tier}>
-                  <Label>{field.tier}</Label>
-                  <Controller
-                    control={control}
-                    name={`sacPrice.${index}.price`}
-                    rules={{
-                      required: true,
-                      min: 0,
-                    }}
-                    render={
-                      ({ field: { onChange, value } }) => (
-                        <NumericFormat
-                          onChange={onChange}
-                          value={value}
-                          type="text"
-                          thousandsGroupStyle="thousand"
-                          thousandSeparator=","
-                          customInput={Input}
-                        />
-                      )
-                    }
-                  />
-                </div>
-              )
-            })
-          }
-        </div>
-        <h3>Pet Candy</h3>
-        <div>
-          {
-            candyPriceFields.map((field, index: number) => {
-              return (              
-                <div key={field.tier}>
-                  <Label>{field.tier}</Label>
-                  <Controller 
-                    control={control}
-                    name={`candyPrice.${index}.price`}
-                    rules={{
-                      required: true,
-                      min: 0,
-                    }}
-                    render={
-                      ({ field: { onChange, value } }) => (
-                        <NumericFormat
-                          onChange={onChange}
-                          value={value}
-                          type="text"
-                          thousandsGroupStyle="thousand"
-                          thousandSeparator=","
-                          customInput={Input}
-                        />
-                      )
-                    }
-                  />
-                </div>
-              )
-            })
-          }
-        </div>
-        <div>
-          <button type="submit">Calculate</button>
-        </div>
-      </form>
+            />
+          </div>
+          <div>
+            <Label htmlFor="statGoal">Stat Goal</Label>
+            <Input
+              id="statGoal"
+              {...form.register(
+                "statGoal", 
+                {
+                  required: true,
+                  min: 0,
+                }
+              )}
+              type="text"
+              inputMode="numeric"
+            />
+          </div>
+          <h2>Prices</h2>
+          <h3>Sac Pets</h3>
+          <div>
+            {
+              sacPriceFields.map((field, index: number) => {
+                return (              
+                  <div key={`sacPrice.${field.tier}`}>
+                    <Label 
+                      className="capitalize"
+                      htmlFor={`sacPrice.${field.tier}`}
+                    >{field.tier}</Label>
+                    <Controller
+                      control={form.control}
+                      name={`sacPrice.${index}.price`}
+                      rules={{
+                        required: true,
+                        min: 0,
+                      }}
+                      render={
+                        ({ field: { onChange, value } }) => (
+                          <NumericFormat
+                            id={`sacPrice.${field.tier}`}
+                            onChange={onChange}
+                            value={value}
+                            type="text"
+                            thousandsGroupStyle="thousand"
+                            thousandSeparator=","
+                            customInput={Input}
+                          />
+                        )
+                      }
+                    />
+                  </div>
+                )
+              })
+            }
+          </div>
+          <h3>Pet Candy</h3>
+          <div>
+            {
+              candyPriceFields.map((field, index: number) => {
+                return (              
+                  <div key={`candyPrice.${field.tier}`}>
+                    <Label 
+                      className="capitalize"
+                      htmlFor={`candyPrice.${field.tier}`}
+                    >{field.tier}</Label>
+                    <Controller 
+                      control={form.control}
+                      name={`candyPrice.${index}.price`}
+                      rules={{
+                        required: true,
+                        min: 0,
+                      }}
+                      render={
+                        ({ field: { onChange, value } }) => (
+                          <NumericFormat
+                            id={`candyPrice.${field.tier}`}
+                            onChange={onChange}
+                            value={value}
+                            type="text"
+                            thousandsGroupStyle="thousand"
+                            thousandSeparator=","
+                            customInput={Input}
+                          />
+                        )
+                      }
+                    />
+                  </div>
+                )
+              })
+            }
+          </div>
+          <div>
+            <button type="submit">Calculate</button>
+          </div>
+        </form>
+      </Form>
     </div>
   )
 }
