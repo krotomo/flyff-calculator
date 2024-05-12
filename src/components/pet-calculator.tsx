883059cbde9b311e9db9ec7356b9671652241df5
Ken Otomo <ken.otomo@stonybrook.edu>
2024-05-12 15:22:59 -0400

    minor style and text changes

--- src/components/pet-calculator.tsx
+++ src/components/pet-calculator.tsx
@@ -37,17 +37,17 @@ export default function PetCalculator() {
       <div className="lg:w-3/4">
         <div className="flex flex-col items-center">
           <img className="w-48" src={flyffLogo}></img>
-          <h1 className="text-center text-4xl font-bold mb-4 pl-4">Pet Cost Calculator</h1>
+          <h1 className="text-center text-4xl font-bold mb-4">Petulator</h1>
         </div>
         <div className="sm:flex">
           <div className="basis-1/2">
-            <h2 className="text-center text-primary text-3xl my-4 font-semibold pl-4">Pet Info</h2>
+            <h2 className="text-primary text-3xl font-semibold my-4 ml-4">Pet Info</h2>
             <PetInput
               setPetState={setPetState}
             ></PetInput>
           </div>
           <div className="basis-1/2">
-            <h2 className="text-center text-primary text-3xl font-semibold my-4 pl-4">Results</h2>
+            <h2 className="text-primary text-3xl font-semibold my-4 ml-4">Results</h2>
             <PetResults
               petType={petType}
               levels={levels!}
@@ -59,7 +59,7 @@ export default function PetCalculator() {
             ></PetResults>
           </div>
         </div>
-        <div className="m-2 text-primary opacity-60">Made by krotomo using assets belonging to Gala Lab Corp</div>
+        <div className="m-4 mt-8 text-primary opacity-60">Made by krotomo using data and assets belonging to Gala Lab Corp</div>
       </div>
     </div>
   )
